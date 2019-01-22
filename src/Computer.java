e972bba26e1f85f98313cb2bf41d759f262d02f2
王小石 <46258000+w1246410140@users.noreply.github.com>
2019-01-22 20:11:47 +0800

    仿真硬件test

--- src/Computer.java
+++ src/Computer.java
@@ -1,14 +1,40 @@
 public class Computer {
-    private int clock;
+    private Clock clock;
+    private CPU cpu;
+}
 
-    Computer(){
-        clock=0;
+class CPU{//CPU类
+    private short addbus;//16位地址线
+    private short databus;//16位数据线
+}
+
+class Clock{//时钟类
+    private int time;//记录系统时间，以毫秒为单位
+
+    Clock(){
+        time=0;//系统时间初始为0
     }
 
-    public void clockstart()throws InterruptedException{
+    public void clockstart()throws InterruptedException{//每隔10毫秒系统时间加10
         for(;;) {
             Thread.sleep(10);
-            clock += 10;
+            time += 10;
         }
     }
+
+    public int gettime(){
+        return time;
+    }
+}
+
+class MMU{
+
+}
+
+class Memory{
+
+}
+
+class Disk{
+
 }
