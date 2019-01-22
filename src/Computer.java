ad5c270dc41fc5a2dec2cddd75fcbbed0d072349
王小石 <46258000+w1246410140@users.noreply.github.com>
2019-01-22 20:42:26 +0800

    仿真硬件  1.22晚demo

--- src/Computer.java
+++ src/Computer.java
@@ -1,9 +1,16 @@
-public class Computer {
+public class Computer {//裸机类
+    private short page_table;//页表基址寄存器，存放页表基址
     private Clock clock;
     private CPU cpu;
 }
 
 class CPU{//CPU类
+    private short PC;//程序计数器
+    private short IR;//指令寄存器
+
+}
+
+class BUS{//总线类
     private short addbus;//16位地址线
     private short databus;//16位数据线
 }
@@ -27,14 +34,14 @@ class Clock{//时钟类
     }
 }
 
-class MMU{
+class MMU{//存储管理部件类
 
 }
 
-class Memory{
+class Memory{//内存类
 
 }
 
-class Disk{
+class Disk{//硬盘类
 
 }
