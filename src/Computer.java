6550b0ddd273088b3227917d856e03efb4797fcd
王小石 <46258000+w1246410140@users.noreply.github.com>
2019-01-23 09:14:39 +0800

    仿真硬件  1.23第一次改

--- src/Computer.java
+++ src/Computer.java
@@ -1,18 +1,15 @@
 public class Computer {//裸机类
-    private short page_table;//页表基址寄存器，存放页表基址
-    private Clock clock;
-    private CPU cpu;
-}
+    short page_table;//页表基址寄存器，存放页表基址
+    private Clock clock;//时钟
+    private CPU cpu;//CPU
+    private BUS bus;//总线
+    private MMU mmu;//存储管理部件
+    private Memory memory;//内存
+    private Disk disk;//硬盘
 
-class CPU{//CPU类
-    private short PC;//程序计数器
-    private short IR;//指令寄存器
-
-}
-
-class BUS{//总线类
-    private short addbus;//16位地址线
-    private short databus;//16位数据线
+    Computer(){//构造函数
+        page_table=0;
+    }
 }
 
 class Clock{//时钟类
@@ -32,6 +29,18 @@ class Clock{//时钟类
     public int gettime(){
         return time;
     }
+}
+
+class CPU{//CPU类
+    private short PC;//程序计数器
+    private short IR;//指令寄存器
+    private int []PSW=new int [2];//指令状态字寄存器
+
+}
+
+class BUS{//总线类
+    private short addbus;//16位地址线
+    private short databus;//16位数据线
 }
 
 class MMU{//存储管理部件类
